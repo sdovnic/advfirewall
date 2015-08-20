cfa727d68c867d5bf11e8bfec63cd8e3ee620fcc
Svenja Dovnic <sdovnic@web.de>
2015-08-21 00:59:39 +0200

    Still trying some type of Notification

--- advfirewall-log-task.cmd
+++ advfirewall-log-task.cmd
@@ -1,7 +1,28 @@
-@echo off
+@echo on
 
 echo %date% %time% %* >> %~dp0\advfirewall-task.log
 
-rem powershell -NoProfile -ExecutionPolicy Bypass -File %~dp0/advfirewall-notify.ps1 %*
+setlocal enableextensions enabledelayedexpansion
+shift
+set advpid=%1
+shift
+set advthreadid=%2
+shift
+set advip=%3
+shift
+set advport=%4
+shift
+set advprotocol=%5
+shift
+set advlocalport=%6
+shift
+set advpath=%7
+rem echo %date% %time% %advpid% %advthreadid% %advip% %advport% %advprotocol% %advlocalport% %advpath% >> %current%\advfirewall-task.log
+rem if not x%advpath:svchost=%==x%advpath% tasklist /svc /fi "pid eq %advpid%" >> %~dp0\advfirewall-task.log
+endlocal
 
-rem msg * /TIME:1 %*
+rem echo %username% >> %~dp0\advfirewall-task.log
+
+rem runas /noprofile /user:rvincent powershell -NoProfile -ExecutionPolicy Bypass -File %~dp0/advfirewall-notify.ps1 %*
+
+rem msg * /Time:5 /Server:%computername% %*
