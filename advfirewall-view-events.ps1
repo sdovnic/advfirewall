c579f477a5288cee643d323eea0d2be529719594
Svenja Dovnic <sdovnic@web.de>
2015-10-02 02:46:58 +0200

    Read from CSV

--- advfirewall-view-events.ps1
+++ advfirewall-view-events.ps1
@@ -1,58 +1,7 @@
 if ($PSVersionTable.PSVersion.Major -lt 3) {
     [string] $PSScriptRoot = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 }
-function Get-LogEntry {
-    param([parameter(Mandatory=$true)] [string] $LogEntry)
-    process {
-        [hashtable] $Protocol = @{
-            1 = "Internet Control Message Protocol (ICMP)";
-            6 = "Transmission Control Protocol (TCP)";
-            17 = "User Datagram Protocol (UDP)";
-            47 = "General Routing Encapsulation (PPTP data over GRE)";
-            51 = "Authentication Header (AH) IPSec";
-            50 = "Encapsulation Security Payload (ESP) IPSec";
-            8 = "Exterior Gateway Protocol (EGP)";
-            3 = "Gateway-Gateway Protocol (GGP)";
-            20 = "Host Monitoring Protocol (HMP)";
-            88 = "Internet Group Management Protocol (IGMP)";
-            66 = "MIT Remote Virtual Disk (RVD)";
-            89 = "OSPF Open Shortest Path First";
-            12 = "PARC Universal Packet Protocol (PUP)";
-            27 = "Reliable Datagram Protocol (RDP)";
-            46 = "Reservation Protocol (RSVP) QoS"
-        }
-        $result = New-Object -TypeName System.Management.Automation.PSObject
-        $result | Add-Member -MemberType NoteProperty -Name "Datum" -Value ([RegEx]::Match($LogEntry, '([\d|.]+)').Groups[1].Value)
-        $result | Add-Member -MemberType NoteProperty -Name "Uhrzeit" -Value ([RegEx]::Match($LogEntry, '\ ([\d|:]+)').Groups[1].Value)
-        $result | Add-Member -MemberType NoteProperty -Name "Prozess" -Value ([RegEx]::Match($LogEntry, '\-pid\ (\d+)\ ').Groups[1].Value)
-        $result | Add-Member -MemberType NoteProperty -Name "Thread" -Value ([RegEx]::Match($LogEntry, '\-threadid\ (\d+)\ ').Groups[1].Value)
-        $result | Add-Member -MemberType NoteProperty -Name "IP Adresse" -Value ([RegEx]::Match($LogEntry,'\-ip\ ([^ ]+)\ ').Groups[1].Value)
-        $result | Add-Member -MemberType NoteProperty -Name "Port" -Value ([RegEx]::Match($LogEntry, '\-port\ (\d+)\ ').Groups[1].Value)
-        [int] $ProtocolNumber = [RegEx]::Match($LogEntry, '\-protocol\ (\d+)\ ').Groups[1].Value
-        $result | Add-Member -MemberType NoteProperty -Name "Protokoll" -Value $ProtocolNumber
-        $result | Add-Member -MemberType NoteProperty -Name "Protokoll Name" -Value ($Protocol[$ProtocolNumber])
-        [string] $Services = [RegEx]::Match($LogEntry,'\-services\ (.*)').Groups[1].Value
-        if ($Services) {
-            $result | Add-Member -MemberType NoteProperty -Name "Pfad" -Value ([RegEx]::Match($LogEntry,'\-path\ (.*)\ (\-)').Groups[1].Value)
-            $result | Add-Member -MemberType NoteProperty -Name "Dienste" -Value $Services
-        } else {
-            $result | Add-Member -MemberType NoteProperty -Name "Pfad" -Value ([RegEx]::Match($LogEntry,'\-path\ (.*)').Groups[1].Value)
-            $result | Add-Member -MemberType NoteProperty -Name "Dienste" -Value ""
-        }
-        return $result
-    }
-}
-if (Test-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath "advfirewall-events.log")) {
-    Write-Host -Object "Lese Ereignisprotokoll Datei"
-    [int] $Counter = 0
-    $LogFile = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "advfirewall-events.log") # -Tail 100
-    [array] $FirewallLog = @()
-    $Total = $Logfile.Count
-    foreach ($LogEntry in $LogFile) {
-        $Counter++
-        Write-Host -Object "Eintrag $Counter von $Total"
-        $FirewallLog += Get-LogEntry -LogEntry $LogEntry
-    }
+if (Test-Path -Path $PSScriptRoot\advfirewall-events.csv) {
     if ($PSVersionTable.PSVersion.Major -lt 3) {
         $WaitHelperSource = @'
 using System;
@@ -137,11 +86,11 @@ namespace Utils
 }
 '@
         Add-Type -TypeDefinition $WaitHelperSource
-        $FirewallLog | Sort-Object -Property Datum, Uhrzeit -Descending | Out-GridView -Title "Windows Firewall Ereignisprotokoll"
+        Import-Csv -Path $PSScriptRoot\advfirewall-events.csv | Sort-Object -Property SystemTime -Descending | Out-GridView -Title "Windows Firewall Ereignisprotokoll"
         $WaitHelper = New-Object -TypeName Utils.WindowHelper
         $WaitHelper.WaitForOutGridViewWindowToClose()
     } else {
-        $FirewallLog | Sort-Object -Property Datum, Uhrzeit -Descending | Out-GridView -Title "Windows Firewall Ereignisprotokoll" -Wait
+        Import-Csv -Path $PSScriptRoot\advfirewall-events.csv | Sort-Object -Property SystemTime -Descending | Out-GridView -Title "Windows Firewall Ereignisprotokoll" -Wait
     }
 } else {
     [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
