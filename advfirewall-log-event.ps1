75c2461789d8e6a5f2f71476df41aa3e62595001
Svenja Dovnic <sdovnic@web.de>
2015-12-29 00:56:13 +0100

    Auto add IP Adresses for Windows Update on Windows 10

--- advfirewall-log-event.ps1
+++ advfirewall-log-event.ps1
@@ -10,9 +10,11 @@ param(
     [string] $DestPort,
     [string] $Protocol
 )
+
 if ($PSVersionTable.PSVersion.Major -lt 3) {
     [string] $PSScriptRoot = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 }
+
 $Event = @{
     SystemTime = $SystemTime
     ThreadID = [int] $ThreadID
@@ -26,6 +28,7 @@ $Event = @{
     Protocol = $Protocol
     Services = [string] (Get-WmiObject -Class Win32_Service -Filter "ProcessID LIKE $ProcessID" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Name)
 }
+
 if ($PSVersionTable.PSVersion.Major -gt 2) {
     Export-Csv -Path (Join-Path -Path $PSScriptRoot -ChildPath "advfirewall-events.csv") -Append -InputObject (New-Object -TypeName PsObject -Property $Event)
 } else {
@@ -38,4 +41,21 @@ if ($PSVersionTable.PSVersion.Major -gt 2) {
     }
     Out-File -FilePath (Join-Path -Path $PSScriptRoot -ChildPath "advfirewall-events.csv") -Append -InputObject $Data
     Remove-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath "advfirewall-events-temp.csv")
+}
+
+if ($Event.Services -match "wuauserv") {
+    if ((Get-WmiObject -Class Win32_OperatingSystem).Caption -match "Windows 10") {
+        $Name = "Windows Update {0}" -f $Event.DestPort
+        if (-not (Get-NetFirewallRule -Name $Name -ErrorAction SilentlyContinue)) {
+            New-NetFirewallRule -Name $Name -DisplayName $Name -Enabled True `
+                                -Profile Any -Direction Outbound -Action Allow `
+                                -LocalAddress Any -RemoteAddress $Event.DestAddress `
+                                -Protocol "TCP" -LocalPort Any -RemotePort $Event.DestPort `
+                                -Program "%SystemRoot%\System32\svchost.exe"
+        } else {
+            [array] $RuleRemoteAddress = (Get-NetFirewallRule -Name $Name | Get-NetFirewallAddressFilter).RemoteAddress
+            [array] $RemoteAddress = ($RuleRemoteAddress) + $Event.DestAddress
+            Set-NetFirewallRule -Name $Name -RemoteAddress $RemoteAddress
+        }
+    }
 }
--- remove-wuauserv-update.cmd
+++
@@ -1,1 +0,0 @@
-@start /min powershell -NoProfile -ExecutionPolicy Bypass .\advfirewall-wuauserv-update-installer.ps1 remove
